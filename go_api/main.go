9f27cdf5a0bd97ef13a758009acbb554ea717aef
pushpendra <ppal36609@gmail.com>
2025-07-02 09:32:36 +0530

    changes

--- go_api/main.go
+++ go_api/main.go
@@ -1,44 +1,20 @@
-
-	package main
+package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
+	fmt.Println("Starting server...")
 
-	fmt.Println("Hello, World!")
 	app := fiber.New()
+
 	app.Get("/abc", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World! 1234")
+		return c.SendString("Hello, world!")
 	})
-	app.Post("/sum", AddOperation)
-	app.Listen(":3000")
 
+	log.Fatal(app.Listen(":3000"))
 }
-
-func AddOperation(c *fiber.Ctx) error {
-	var request AddOperationRequest
-	if err := c.BodyParser(&request); err != nil {
-		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
-			"error": "Invalid request body",
-		})
-	}
-
-	result := request.Num1 + request.Num2
-	response := AddOperationResponse{Result: result}
-
-	return c.JSON(response) 
-}
-
-type AddOperationRequest struct {
-	Num1 int json:"num1"
-	Num2 int json:"num2"
-}
-
-type AddOperationResponse struct {
-	Result int json:"result"
-}
-	
